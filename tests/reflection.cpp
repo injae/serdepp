b8307d2147295b0825268ea7a0e0d0ed73366fa8
injae <8687lee@gmail.com>
2024-01-01 16:36:11 +0900

    add test reflection struct -> tuple

--- tests/reflection.cpp
+++ tests/reflection.cpp
@@ -10,4 +10,8 @@ TEST_CASE("3: reflection struct (pass)", "[multi-file:3]") {
           std::tuple<std::string, int, std::vector<std::string>,
                      std::optional<std::string>, std::optional<std::string>,
                      std::map<std::string, std::string>, nested>>);
+  REQUIRE(serde::type_info<test>.size == 7);
+  REQUIRE(serde::tuple_size_v<test> == 7);
+  REQUIRE(std::is_same_v<std::string,
+                         std::tuple_element_t<0, serde::to_tuple_t<test>>>);
 }
